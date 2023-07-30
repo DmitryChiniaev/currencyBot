429f9f6a7305e31faaa6ef5bd8ed733fda94fd04
Dmitry <dmitrychinyaev93@yandex.ru>
2023-07-30 23:46:10 +0300

    fasterXML deployed

--- src/main/java/com/dmitrychinyaev/currencybot/entity/Valute.java
+++ src/main/java/com/dmitrychinyaev/currencybot/entity/Valute.java
@@ -1,15 +1,18 @@
 package com.dmitrychinyaev.currencybot.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
-public class ForeignCurrency {
+@Setter
+@JsonIgnoreProperties(value = {"ID","NumCode"})
+public class Valute {
     private String charCode;
     private int nominal;
     private String name;
     private double value;
 }
+
